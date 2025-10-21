aa1df82c22965755159195057092d94114e6ec2f
Bruno Serrate <brunostj@hotmail.com>
2025-10-21 06:33:55 -0300

    fix: ajuste CORS para a API oficial (prod)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const loadEnv = require('./src/config/env');
 loadEnv();
 const routes = require('./src/routes');
+const path = require('path');
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
@@ -14,11 +15,14 @@ if (process.env.APP_ENV === 'development') {
     origin: [
       "http://localhost:8080",
       "http://localhost:8000",
+      "http://localhost:80",
+      "http://localhost",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"]
   }));
 }
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use('/api', routes);
 
